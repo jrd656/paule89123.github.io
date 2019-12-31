a46edda4b3363c3b9570b7117ed761611f1e7695
Paul Evans <paulevans89@gmail.com>
2019-12-31 20:03:33 +0000

    javascript changed to make the carousel work by using the translateX function, rather than a simple show/hide of the individual images. This allows animation between the images

--- carousel.js
+++ carousel.js
@@ -1,22 +1,38 @@
-var photoIndex = 0;
-var photos = document.querySelectorAll(".photo");
+let photoIndex = 1;
+const carousel = document.querySelector(".carousel");
+const photos = document.querySelectorAll(".photo");
+const photoWidth = photos[0].getBoundingClientRect().width;
+const prevBtn = document.querySelector("#leftArrowContainer");
+const nextBtn = document.querySelector("#rightArrowContainer");
 
-function showPhoto(index) {
-  for (let i = 0; i < photos.length; i++) {
-    photos[i].style.display = "none";
-  }
-  photos[index].style.display = "block";
-}
-
-showPhoto(photoIndex);
+prevBtn.addEventListener("click", function(){changePhoto(-1)});
+nextBtn.addEventListener("click", function(){changePhoto(1)});
+carousel.addEventListener("transitionend", reset);
 
 function changePhoto(n) {
   photoIndex += n;
-  if (photoIndex == photos.length) {
-    photoIndex = 0;
-  }
-  if (photoIndex < 0) {
-    photoIndex = photos.length-1;
-  }
-  showPhoto(photoIndex);
+  carousel.style.transition = "450ms transform ease";
+  const translationAmount = -photoWidth * photoIndex;
+  carousel.style.transform = `translateX(${translationAmount}px)`;
+  console.log(photoIndex);
 }
+
+// Reset the carousel position when the beginning or end of the carousel is reached:
+function reset() {
+  if (photoIndex <= 0) {
+    photoIndex = photos.length-2;
+    const translationAmount = -photoWidth * photoIndex;
+    carousel.style.transform = `translateX(${translationAmount}px)`;
+    carousel.style.transition = "none";
+  }
+  if (photoIndex >= photos.length-1) {
+    photoIndex = 1;
+    const translationAmount = -photoWidth * photoIndex;
+    carousel.style.transform = `translateX(${translationAmount}px)`;
+    carousel.style.transition = "none";
+  }
+}
+
+// Set starting position of carousel:
+const translationAmount = -photoWidth * photoIndex;
+carousel.style.transform = `translateX(${translationAmount}px)`;
