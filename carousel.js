999896173e3f0e782de70d38fe173d6c1b3a58da
Paul Evans <paulevans89@gmail.com>
2019-12-31 22:38:52 +0000

    made carousel respond to left and right keys, made carousel play automatically, added play/pause functionality, styled play/pause buttons

--- carousel.js
+++ carousel.js
@@ -4,28 +4,35 @@ const photos = document.querySelectorAll(".photo");
 const photoWidth = photos[0].getBoundingClientRect().width;
 const prevBtn = document.querySelector("#leftArrowContainer");
 const nextBtn = document.querySelector("#rightArrowContainer");
+const pauseBtn = document.querySelector("#pause");
+const playBtn = document.querySelector("#play");
+let paused = false;
 
 prevBtn.addEventListener("click", function(){changePhoto(-1)});
 nextBtn.addEventListener("click", function(){changePhoto(1)});
 carousel.addEventListener("transitionend", reset);
+pauseBtn.addEventListener("click", pause);
+playBtn.addEventListener("click", play);
 
 function changePhoto(n) {
+  if (photoIndex <= 0) {return}
+  if (photoIndex >= photos.length-1) {return}
+
   photoIndex += n;
-  carousel.style.transition = "450ms transform ease";
+  carousel.style.transition = "580ms transform ease-in-out";
   const translationAmount = -photoWidth * photoIndex;
   carousel.style.transform = `translateX(${translationAmount}px)`;
-  console.log(photoIndex);
 }
 
 // Reset the carousel position when the beginning or end of the carousel is reached:
 function reset() {
-  if (photoIndex <= 0) {
+  if (photoIndex === 0) {
     photoIndex = photos.length-2;
     const translationAmount = -photoWidth * photoIndex;
     carousel.style.transform = `translateX(${translationAmount}px)`;
     carousel.style.transition = "none";
   }
-  if (photoIndex >= photos.length-1) {
+  if (photoIndex === photos.length-1) {
     photoIndex = 1;
     const translationAmount = -photoWidth * photoIndex;
     carousel.style.transform = `translateX(${translationAmount}px)`;
@@ -36,3 +43,28 @@ function reset() {
 // Set starting position of carousel:
 const translationAmount = -photoWidth * photoIndex;
 carousel.style.transform = `translateX(${translationAmount}px)`;
+
+// Left and right keys:
+document.onkeydown = function(event) {
+  if (event.keyCode == 37) {
+    changePhoto(-1);
+  }
+  if (event.keyCode == 39) {
+    changePhoto(1);
+  }
+}
+
+// Automatically start slideshow:
+let interval = setInterval(changePhoto, 3000, 1);
+
+function pause() {
+  clearInterval(interval);
+  paused = true;
+}
+
+function play() {
+  if (paused === true) {
+  setInterval(changePhoto, 3000, 1);
+  paused = false;
+  }
+}
