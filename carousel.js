e9a1b5224e93ed30967e386ec15b238fabfef2e0
Paul Evans <paulevans89@gmail.com>
2020-01-01 19:29:05 +0000

    made the site responsive by adding media queries in CSS and an event listener in JS that listens for a window resize event to make the carousel responsive. Also tweaked the position and sizes of various elements to add finishing touches to the site

--- carousel.js
+++ carousel.js
@@ -1,7 +1,8 @@
 let photoIndex = 1;
 const carousel = document.querySelector(".carousel");
+const carouselContainer = document.querySelector(".carousel-container");
 const photos = document.querySelectorAll(".photo");
-const photoWidth = photos[0].getBoundingClientRect().width;
+let photoWidth = carouselContainer.getBoundingClientRect().width;
 const prevBtn = document.querySelector("#leftArrowContainer");
 const nextBtn = document.querySelector("#rightArrowContainer");
 const pauseBtn = document.querySelector("#pause");
@@ -10,28 +11,37 @@ let paused = false;
 
 prevBtn.addEventListener("click", () => {
   changePhoto(-1);
-  clearInterval(interval);
-  interval = setInterval(changePhoto, 2500, 1);
+  if (paused == false) {
+    clearInterval(interval);
+    interval = setInterval(changePhoto, 2500, 1);
+    }
   });
 
 nextBtn.addEventListener("click", () => {
   changePhoto(1);
-  clearInterval(interval);
-  interval = setInterval(changePhoto, 2500, 1);
+  if (paused == false) {
+    clearInterval(interval);
+    interval = setInterval(changePhoto, 2500, 1);
+    }
   });
 
 carousel.addEventListener("transitionend", reset);
 pauseBtn.addEventListener("click", pause);
 playBtn.addEventListener("click", play);
 
-function changePhoto(n) {
+function changePhoto(n, noTransition) {
   if (photoIndex <= 0) {return}
   if (photoIndex >= photos.length-1) {return}
 
-  photoIndex += n;
+  carousel.style.transition = "none";
+
+  if (noTransition != 1) {
   carousel.style.transition = "580ms transform ease-in-out";
+  }
+
+  photoIndex += n;
   const translationAmount = -photoWidth * photoIndex;
-  carousel.style.transform = `translateX(${translationAmount}px)`;
+  carousel.style.transform = `translate(${translationAmount}px)`;
 }
 
 // Reset the carousel position when the beginning or end of the carousel is reached:
@@ -39,32 +49,36 @@ function reset() {
   if (photoIndex === 0) {
     photoIndex = photos.length-2;
     const translationAmount = -photoWidth * photoIndex;
-    carousel.style.transform = `translateX(${translationAmount}px)`;
+    carousel.style.transform = `translate(${translationAmount}px)`;
     carousel.style.transition = "none";
   }
   if (photoIndex === photos.length-1) {
     photoIndex = 1;
     const translationAmount = -photoWidth * photoIndex;
-    carousel.style.transform = `translateX(${translationAmount}px)`;
+    carousel.style.transform = `translate(${translationAmount}px)`;
     carousel.style.transition = "none";
   }
 }
 
 // Set starting position of carousel:
 const translationAmount = -photoWidth * photoIndex;
-carousel.style.transform = `translateX(${translationAmount}px)`;
+carousel.style.transform = `translate(${translationAmount}px)`;
 
 // Left and right keys:
 document.onkeydown = function(event) {
   if (event.keyCode == 37) {
     changePhoto(-1);
-    clearInterval(interval);
-    interval = setInterval(changePhoto, 2500, 1);
+      if (paused == false) {
+        clearInterval(interval);
+        interval = setInterval(changePhoto, 2500, 1);
+        }
   }
   if (event.keyCode == 39) {
     changePhoto(1);
-    clearInterval(interval);
-    interval = setInterval(changePhoto, 2500, 1);
+      if (paused == false) {
+        clearInterval(interval);
+        interval = setInterval(changePhoto, 2500, 1);
+        }
   }
 }
 
@@ -81,4 +95,13 @@ function play() {
   interval = setInterval(changePhoto, 2500, 1);
   }
   paused = false;
+}
+
+// Making the carousel responsive:
+window.addEventListener("resize", changeWidth);
+
+function changeWidth() {
+  let originalWidth = photoWidth;
+  photoWidth = carouselContainer.getBoundingClientRect().width;
+  if (originalWidth != photoWidth) {changePhoto(0, 1)};
 }
